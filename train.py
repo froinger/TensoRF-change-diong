38596ebf681c5d4a565a9ee4e89a9c068375f68d
froinger <151984931+froinger@users.noreply.github.com>
2024-04-30 14:32:29 +0800

    Update train.py

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def reconstruction(args):
     reso_cur = N_to_reso(args.N_voxel_init, aabb)
     nSamples = min(args.nSamples, cal_n_samples(reso_cur,args.step_ratio))
 
-# 123
+# 12345
     if args.ckpt is not None:
         ckpt = torch.load(args.ckpt, map_location=device)
         kwargs = ckpt['kwargs']
